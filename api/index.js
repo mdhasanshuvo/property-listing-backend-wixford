421193d1490acd8a4a3e4a4c9fe1671c969e53e1
Mohammed Hasan <mdhasanshuvo220@gmail.com>
2026-01-11 19:22:29 +0600

    fix: serverless database connection and dynamic swagger server selection

--- api/index.js
+++ api/index.js
@@ -1,3 +1,18 @@
+require('dotenv').config();
 const app = require('../src/app');
+const { connectDB } = require('../src/config/db');
 
-module.exports = app;
+// Cache the database connection
+let isConnected = false;
+
+// Connect to MongoDB before handling requests
+const handler = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  
+  return app(req, res);
+};
+
+module.exports = handler;
